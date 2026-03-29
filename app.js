9256662acd8fbd3dc0f77c216e5c662f99b63e8e
Gopu Rama Krishna Reddy <rkreddyg115@gmail.com>
2026-03-30 00:17:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 function sum(a,b){
-    console.log("the sum of a and b is "+(a+b));
     return a+b;
 }
 
 function substract(a,b){
-    console.log("the substraction of a and b is "+(a-b));
     return a-b;
 }
