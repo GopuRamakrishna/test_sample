152f29e53ff93fc8c5faee55fdd8674fb815c926
GopuRamakrishna <rkreddyg115@gmail.com>
2026-04-01 00:03:30 +0530

    sample testing pro

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log("this is a sampele JS file");
 function sum(a,b){
     console.log("hey this is the sum function");
     return a+b;
+    console.log("this is statement will never excute");
 }
 console.log("this is simple sub function");
 function substract(a,b){
